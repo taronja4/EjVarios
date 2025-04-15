7594410d5b87bd5b38bc108b0162792f786f676d
Adrian Garcia Sansano <153595761+taronja4@users.noreply.github.com>
2025-04-15 17:45:52 +0200

    Update ejerciciosVarios.java

--- ejerciciosVarios.java
+++ ejerciciosVarios.java
@@ -1,15 +1,17 @@
+import java.time.LocalDate;
 import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class ejerciciosVarios {
     public static void main(String[] args) {
-        //a
+        //1
         Consumer<String> cunsum= xd -> System.out.println(xd);
         cunsum.accept("Estoy en clase de programacion");
 
-        //b
+        //2
         System.out.println("--------Lista enteros---------");
         List<Integer> numos=new ArrayList<>();
         Consumer<Integer> paco=xd-> System.out.print(xd+" ");
@@ -19,7 +21,7 @@ public class ejerciciosVarios {
         numos.forEach(paco);
         System.out.println();
 
-        //c
+        //3
         System.out.println("--------Ultima letra------");
         List<String> ult=new ArrayList<>();
 
@@ -33,7 +35,7 @@ public class ejerciciosVarios {
         ult.forEach(con);
         System.out.println();
 
-        //d
+        //7,8
         System.out.println("--------Multiplicacion----------");
         BiConsumer<Integer,Integer> multi=(a,b)-> System.out.println(a*b);
         multi.accept(-9,7);
@@ -47,7 +49,12 @@ public class ejerciciosVarios {
         BiConsumer<Integer,Integer> resta=(a,b)-> System.out.println(a-b);
         resta.accept(-9,7);
 
-
+        //9
+        calculator(-9,7,suma);
+        calculator(-9,7,resta);
+        calculator(-9,7,multi);
+        calculator(-9,7,divi);
+        //10
         System.out.println("--------------Listeo----------");
         Map<Integer,String> mapeo=new LinkedHashMap<>();
         BiConsumer<Integer,String> nombre2= (n,s)-> System.out.println(n+" "+s);
@@ -57,13 +64,25 @@ public class ejerciciosVarios {
         mapeo.put(3,"tres");
         mapeo.put(5,"cinco");
         mapeo.forEach(nombre2);
-
+        //4
         System.out.println("-------------java----------");
         Supplier<String> ramon=() -> "Java es un lenguaje de programacion";
         System.out.println(ramon.get());
-
+        //5
         System.out.println("----------numero--------");
         Supplier<Double> xd=()-> Math.random();
         System.out.println(xd.get());
+
+        //11
+        LocalDate fechaActual = LocalDate.now();
+        System.out.println("Fecha actual: " + fechaActual);
+        //12
+        Programador p=new Programador("Paco",1233,LocalDate.now());
+        Consumer<Programador> p3=(a)-> System.out.println(a);
+        p3.accept(p);
+    }
+
+    public static void calculator(int a ,int b,BiConsumer<Integer,Integer> op){
+        op.accept(a,b);
     }
 }
